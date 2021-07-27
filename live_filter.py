4516c3db44ae9ebd55c9d913a2cce148dd1f9107
ayushkumar2000 <akak355@gmail.com>
2021-07-27 07:34:26 +0530

    added signal_filter to replace lfilter

--- live_filter.py
+++ live_filter.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import struct
 import scipy
-import math
+from math import ceil
 from scipy.io import wavfile as wav
 import freq_gen as fg
 import filter as ft
@@ -15,6 +15,10 @@ def live_run(seconds,save_file = False):
     chanels = 1
     fs = 22050
     filename = "audio_files/recording1.wav"
+    #first find the filter to run
+    filter_num,arg_list = ft.filter_finder()
+    #visualize the filter
+    ft.visualize_filter(filter_num,arg_list)
     pa = pyaudio.PyAudio() 
     stream1 = pa.open(format=sample_format, channels=chanels,
                     rate=fs, input=True,frames_per_buffer=chunk)
@@ -25,42 +29,31 @@ def live_run(seconds,save_file = False):
     tot_samples = np.array([])
     tot_filtered_samples = np.array([])
 
-    #first find the filter to run
-    filter_num,arg_list = ft.filter_finder()
-    #visualize the filter
-    ft.visualize_filter(filter_num,arg_list)
+    
 
     print('Recording...')
-    for i in range(int(seconds/5)):
+    for i in range(int(ceil(seconds/5))):
         data = stream1.read(chunk)
         #process data to the integer
         samples = []
 
         samples = np.frombuffer(data, dtype='int16')
-
-        #filter the samples
-        # filtered_samples = samples
-        filtered_samples = ft.filter_runner(samples,filter_num,arg_list)
-        
-        
-        #save the filtered samples into output.wav
-        # fg.play(filtered_samples,fs,True)
-        #play the output.wav file
-        # fs,final_samples = fg.file_samples('audio_files/output.wav')
-
-        
-        #
-        filtered_samples = filtered_samples.astype(np.int16)
+                
         if(not save_file):
+            filtered_samples = ft.filter_runner(samples,filter_num,arg_list)
+            filtered_samples = filtered_samples.astype(np.int16)
             stream2.write(filtered_samples.tobytes())
         else:
             tot_samples = np.append(tot_samples,samples)
 
     
     if(save_file):
+        print("Recording completed")
+        print("Applying filter ....")
         tot_filtered_samples = ft.filter_runner(tot_samples,filter_num,arg_list)
         fg.play(tot_samples,fs,True,'audio_files/original.wav')
         fg.play(tot_filtered_samples,fs,True,'audio_files/filtered.wav')
+        print("Done")
 
 
 
